b4e441109e948ba5d39e9a3151d121925a51a15e
saul corona <saulcorona@MacBook-Air-de-saul.local>
2021-05-22 12:01:55 -0500

    Refactor code

--- src/main/Leer.java
+++ src/main/Leer.java
@@ -1,6 +1,8 @@
 package main;
 import java.io.*;
 
+//Validaciones
+
 public class Leer {
     //1.-
     public static String dato(){
@@ -24,41 +26,12 @@ public class Leer {
     }
 
     //3.-
-    public static float datoFloat(){
-        try {
-            Float f = new Float(dato());
-            return (f.floatValue());
-        } catch (NumberFormatException error){
-            return (Float.NaN);
-        }
-    }
-
-    //4.-
     public static double datoDouble(){
         try{
             Double d = new Double(dato());
             return (d.doubleValue());
         } catch (NumberFormatException error){
             return (Double.NaN);
-        }
-    }
-
-    //5.-
-    public static char datoChar(){
-        try{
-            char resp = dato().charAt(0);
-            return resp;
-        } catch (Exception error){
-            return ('z');
-        }
-    }
-
-    //6.-
-    public static boolean datoBoolean(){
-        try{
-            return (Boolean.parseBoolean(dato()));
-        } catch (NumberFormatException error){
-            return false;
         }
     }
 }
